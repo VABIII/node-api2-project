b247fb2ad31bc7ce876c3f1797e9e0ba12d24812
VABIII <tbengtson82@gmail.com>
2022-01-11 13:40:16 -0600

    Put request ep working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -47,7 +47,6 @@ router.post('/', async (req, res) => {
         } else {
             res.status(201).json(newPost)
         }
-
     }
     catch (err) {
         res.status(500).json({
@@ -55,12 +54,33 @@ router.post('/', async (req, res) => {
             error: err.message,
         })
     }
-
-
-
 })
 
+router.put('/:id', async (req,res) => {
+    const { id } = req.params
+    const {title, contents} = req.body
 
+    try {
+        const updatedPost = await Posts.update(id, req.body)
+        if(!updatedPost) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            })
+        } else if (!title || !contents) {
+            res.status(400).json({
+                message: "Please provide title and contents or the post"
+            })
+        } else {
+            res.status(200).json(updatedPost)
+        }
+    }
+    catch (err) {
+        res.status(500).json({
+            message: 'There was an error while saving the post to the database',
+            error: err.message,
+        })
+    }
+})
 
 
 
