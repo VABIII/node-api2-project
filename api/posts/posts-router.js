e8116a6299f6918562f2411228e8d796f4ffa167
VABIII <tbengtson82@gmail.com>
2022-01-11 13:12:50 -0600

    Get post by id endpoint working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -16,7 +16,25 @@ router.get('/', async (req, res) => {
     }
 })
 
-
+router.get('/:id', async (req, res) => {
+    const {id} = req.params
+    try {
+        const post = await Posts.findById(id)
+        if(!post) {
+            res.status(404).json({
+                message: 'The post with the specified ID does not exist'
+            })
+        } else {
+            res.json(post)
+        }
+    }
+    catch (err) {
+        res.status(500).json({
+            message: 'The post information could not be retrieved',
+            error: err.message
+        })
+    }
+})
 
 
 
