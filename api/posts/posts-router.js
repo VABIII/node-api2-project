3a330a2bbf9cf6b1984ea98607b756a120942083
VABIII <tbengtson82@gmail.com>
2022-01-11 13:03:17 -0600

    Get all posts endpoint working

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -2,7 +2,19 @@ const express = require('express')
 const Posts = require('./posts-model')
 const router = express.Router()
 
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.find()
+        res.json(posts)
 
+    }
+    catch (err) {
+        res.status(500).json({
+            message: 'The posts information could not be retrieved',
+            error: err.message,
+        })
+    }
+})
 
 
 
