ce258b3eb68e8185655c02501ecd4adbbb17a6bc
VABIII <tbengtson82@gmail.com>
2022-01-11 12:56:37 -0600

    Router being pulled into server file

--- api/server.js
+++ api/server.js
@@ -1,13 +1,19 @@
 const express = require('express')
+const postsRouter = require('./posts/posts-router')
 
 const server = express()
 
 server.use(express.json())
 
+server.use('/api/posts', postsRouter)
 
 
-
-
+server.get('*', (req, res) => {
+    res.send(`
+        <h2>Shelter API</h2>
+        <p>Welcome to the Shelter API</p>
+    `)
+})
 
 
 
